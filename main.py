d36f52d072c9d2f2ebdddb758e0ba22a2b7dc4dd
jlairforce1 <115430356+jlairforce1@users.noreply.github.com>
2022-10-10 17:22:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def hello_world():
 
 @app.route("/hello")
 def index():
-    flash("What's your name")
+    flash("What's your name?")
     return render_template("index.html")
 
 @app.route("/greet", methods=["POST", "GET"])
